dcf89a4ca98afbad6097f8e98cc9589988a3f514
skfarukhossain1998 <127541802+skfarukhossain1998@users.noreply.github.com>
2023-04-14 18:58:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
-  //   write your code here
+  return key ;
 }
 
 // Do not change the code below
